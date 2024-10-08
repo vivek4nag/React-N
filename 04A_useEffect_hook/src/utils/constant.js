f59de044677269a8d2c58026dd2902e56f1b66e9
viek4nag <vivek4nag@gmail.com>
2024-10-08 15:52:12 +0530

    API calls, CORS, useEffect Hook

--- 04A_useEffect_hook/src/utils/constant.js
+++ 04A_useEffect_hook/src/utils/constant.js
@@ -6,12 +6,14 @@
 //   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9046136&lng=77.614948&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 
+// https://proxy.cors.sh/
+// above link is to bypass CORS. append it beofrer the API link. but it is not working properly everytime
 
 // Swiggy API for to get Restaurant data
-export const SWIGGY_API_URL = `https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9046136&lng=77.614948&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
+export const SWIGGY_API_URL = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9046136&lng=77.614948&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
 
 //  Swiggy API for to get Restaurant Item 
-export const MENU_API_URL = `https://proxy.cors.sh/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9046136&lng=77.614948&restaurantId=`;
+export const MENU_API_URL = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9046136&lng=77.614948&restaurantId=`;
 
 // Restaurant Item Image CDN URL for Restaurant card
 export const IMG_CDN_URL = `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/`;
