92eb3603b11826e29208afb9fe404fc4505898a3
viek4nag <vivek4nag@gmail.com>
2024-10-11 17:52:14 +0530

    class based components, life cycle of CBC

---
+++ 06_class_Based_Components/README.md
@@ -0,0 +1,17 @@
+2 types of exports & imports 
+
+- Default Export/Import
+export default component
+import component from "path";
+
+
+- Named Export/Import
+export const Component
+import {Component} from "path"
+
+
+- React Hooks - see notes
+
+react fiber architecture =  (<https://github.com/acdlite/react-fiber-architecture>)
+
+react lifecycle method diagram = <https://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/>
---
+++ 06_class_Based_Components/src/components/Header.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+// import foodfFireLogo from "../../../public/Images";
+// import { FaCartArrowDown } from "react-icons/fa";
+
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  console.log("header rendered"); // just to show that button click krne pr bhi pura heeader firse render ho rha hai but bcz of reconciliation & diffing algo which use virtual DOM, react is supaa faast to render all these stuff
+
+// if no dependency array , useeffect is called on every render
+  // if dependency array is empty=[], useeffect is called only once at initial render
+  // if dependecy array is [login] i.e. the login button . it will be called everytime that login button is updated
+  useEffect(() => {
+    console.log("useeffect called");
+  }, [isLoggedIn]);
+
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <Link to="/">
+          {/* <img className="logo" src={foodfFireLogo} alt="Tasty Trails Logo" /> */}
+        </Link>
+      </div>
+
+      <div className="nav-items">
+        <ul>
+          <li><Link className="nav-links" to="/">Home</Link></li>
+          <li><Link className="nav-links" to="/about">About</Link></li>
+          <li><Link className="nav-links" to="/contact">Contact</Link></li>
+          {/* <li><Link className="nav-links"><FaCartArrowDown /></Link></li> */}
+          <button
+            className="login"
+            onClick={() => setIsLoggedIn(!isLoggedIn)}
+          >
+            {isLoggedIn ? "Logout" : "Login"}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
